6b1027b22720597de98fc93bba0bfa48fdcf710f
1franck <1209308+1franck@users.noreply.github.com>
2026-07-25 10:50:00 -0400

    refactor: rename Outcome types to Result and add ResultValue types

--- packages/kofono/src/common/result.ts
+++ packages/kofono/src/common/result.ts
@@ -1,66 +1,49 @@
-// export interface Result<TSuccessValue, TError = Error> {
-//     value: TSuccessValue;
-//     error?: TError;
-//     errorValue?: string;
-//     ok(): boolean;
-//     fail(): boolean;
-// }
-//
-// export class SuccessResult<TSuccessValue> implements Result<TSuccessValue> {
-//     value: TSuccessValue;
-//     error: undefined;
-//
-//     constructor(value: TSuccessValue) {
-//         this.value = value;
-//         this.error = undefined;
-//     }
-//
-//     ok(): boolean {
-//         return true;
-//     }
-//
-//     fail(): boolean {
-//         return false;
-//     }
-// }
-//
-// export class FailResult<TError = Error> implements Result<undefined, TError> {
-//     value: undefined;
-//     error?: TError;
-//
-//     get errorValue(): string {
-//         return (this.error as Error)?.message || "";
-//     }
-//
-//     constructor(error: TError | string) {
-//         this.error =
-//             typeof error === "string" ? (new Error(error) as TError) : error;
-//     }
-//
-//     ok(): boolean {
-//         return false;
-//     }
-//
-//     fail(): boolean {
-//         return true;
-//     }
-// }
+/**
+ * Simple Result true/false
+ */
+export type Result<TError = string> = OkResult | FailResult<TError>;
 
-export type OkOutcome = {
+export type OkResult = {
     ok: true;
 };
 
-export type FailOutcome<TError = Error> = {
+export type FailResult<TError = string> = {
     ok: false;
     error: TError;
 };
 
-export type Outcome<TError = Error> = OkOutcome | FailOutcome<TError>;
-
-export const outcome = Object.freeze({
-    ok: (): OkOutcome => ({ ok: true }),
-    fail: <TError = Error>(error: Error | string): FailOutcome<TError> => ({
+export const result = Object.freeze({
+    ok: (): OkResult => ({ ok: true }),
+    fail: <TError = string>(error: TError): FailResult<TError> => ({
         ok: false,
-        error: (typeof error === "string" ? new Error(error) : error) as TError,
+        error,
+    }),
+});
+
+/**
+ * Result with value or error
+ */
+export type ResultValue<TValue = any, TError = string> =
+    | OkResultValue<TValue>
+    | FailResultValue<TError>;
+
+export type OkResultValue<TValue> = {
+    ok: true;
+    value: TValue;
+};
+
+export type FailResultValue<TError = string> = {
+    ok: false;
+    error: TError;
+};
+
+export const resultValue = Object.freeze({
+    ok: <TValue = any>(value: TValue): OkResultValue<TValue> => ({
+        ok: true,
+        value,
+    }),
+    fail: <TError = string>(error: TError): FailResultValue<TError> => ({
+        ok: false,
+        error,
     }),
 });
