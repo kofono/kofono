b2ed5090db55add79bb02958dced76e1fc958111
1franck <1209308+1franck@users.noreply.github.com>
2026-07-26 11:33:52 -0400

    feature: deprecate FormSelectors

--- packages/kofono/src/form/FormSelectors.ts
+++ packages/kofono/src/form/FormSelectors.ts
@@ -1,31 +1,10 @@
 import { answerablePropertyTypes } from "../property/categories";
 import { TreeType } from "../property/types";
-import { DataSelector } from "../selector/DataSelector";
 import type { Form } from "./Form";
 
+// deprecated
 export class FormSelectors {
     public constructor(private form: Form) {}
-
-    public getRootSelectors(): string[] {
-        const selectors: string[] = [];
-        for (const selector of this.form.propsKeys()) {
-            if (!selector.includes(DataSelector.separator)) {
-                selectors.push(selector);
-            }
-        }
-        return selectors;
-    }
-
-    public getChildrenSelectors(parent: string): string[] {
-        parent = parent + DataSelector.separator;
-        const selectors: string[] = [];
-        for (const selector of this.form.propsKeys()) {
-            if (selector.startsWith(parent) && selector !== parent) {
-                selectors.push(selector);
-            }
-        }
-        return selectors;
-    }
 
     public getLeaf(): string[] {
         const selectors: string[] = [];
