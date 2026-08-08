dbb7deea69ea81e11e0806e4e23a210abb2ce8cc
1franck <1209308+1franck@users.noreply.github.com>
2026-08-08 13:58:33 -0400

    chore: update

--- packages/kofono/src/validator/regexp/IntegerValidator.spec.ts
+++ packages/kofono/src/validator/regexp/IntegerValidator.spec.ts
@@ -6,10 +6,10 @@ describe("integer validator", () => {
     let form: Form;
     it("test integer regex", async () => {
         form = await K.form({
-            a: K.number("integer").default(3.14),
+            a: K.number("integer"), // default value is 0
         });
-        expect(form.isPropValid("a")).toBeFalsy();
-        await form.update("a", 4);
         expect(form.isPropValid("a")).toBeTruthy();
+        await form.update("a", 3.14);
+        expect(form.isPropValid("a")).toBeFalsy();
     });
 });
