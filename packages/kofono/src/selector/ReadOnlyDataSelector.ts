311094833affe85d2e84f21a7155a6e7950d6902
1franck <1209308+1franck@users.noreply.github.com>
2026-08-09 20:45:44 -0400

    fix: correct method signature for _set and _delete

--- packages/kofono/src/selector/ReadOnlyDataSelector.ts
+++ packages/kofono/src/selector/ReadOnlyDataSelector.ts
@@ -2,11 +2,11 @@ import { DataSelector } from "./DataSelector";
 import { ReadOnlySelectorError } from "./ReadOnlySelectorError";
 
 export class ReadOnlyDataSelector extends DataSelector {
-    protected _delete(selector: string): void {
+    protected _delete(selector: string, _data: any): void {
         throw new ReadOnlySelectorError("delete", selector);
     }
 
-    protected _set(selector: string): void {
+    protected _set(selector: string, _value: unknown, _data: any): void {
         throw new ReadOnlySelectorError("set", selector);
     }
 }
